c9e48389f94e92631a256164164cf75c15689367
Lucas Stofaleti <lucastofaleti@hotmail.com>
2021-01-20 00:01:20 -0300

    Classe: UsedProduct

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ImportedProduct extends Product
+{
+	protected double customsFee;
+
+	public ImportedProduct(String name, double price, double customsFee) 
+	{
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public double getCustomsFee() 
+	{
+		return customsFee;
+	}
+	public void setCustomsFee(double customsFee) 
+	{
+		this.customsFee = customsFee;
+	}
+	
+	public double totalPrice()
+	{
+		return price + customsFee;
+	}
+	
+	@Override
+	public String priceTag()
+	{
+		return this.name +
+				" $ " +
+				String.format("%.2f", this.totalPrice()) + 
+				"(Customs fee: $ " + String.format("%.2f", this.customsFee);	
+	}
+	
+}
